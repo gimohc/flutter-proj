74d7eb7b827760730d806fe015a827452bd15e9f
gimohc <mrlenovog50@gmail.com>
2025-11-13 21:05:48 +0300

    Created google authentication button

---
+++ lib/components/auth/google_auth.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class GoogleAuth extends StatelessWidget {
+  const GoogleAuth({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsetsGeometry.only(bottom: 20),
+      child: ElevatedButton(
+        onPressed: () => {},
+        style: ElevatedButton.styleFrom(
+          foregroundColor: Colors.black,
+          backgroundColor: Colors.white,
+          minimumSize: const Size(double.infinity, 48),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
+            side: const BorderSide(color: Colors.grey),
+          ),
+          elevation: 2,
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              'assets/images/google_logo.png',
+              height: 24.0,
+              width: 24.0,
+            ),
+            const SizedBox(width: 10),
+            const Text('Continue with Google', style: TextStyle(fontSize: 16)),
+          ],
+        ),
+      ),
+    );
+  }
+}
