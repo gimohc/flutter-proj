75632663c2ef1349584bf9c88cce602f015982c4
gimohc <mrlenovog50@gmail.com>
2025-11-18 13:34:19 +0300

    added profile button

--- lib/components/default_page.dart
+++ lib/components/default_page.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:proj/components/profile_button.dart';
 
 class DefaultPage extends StatelessWidget {
   final String? title;
   final Widget child;
+  final bool? showProfile;
 
-  const DefaultPage({super.key, this.title, required this.child});
+  const DefaultPage({
+    super.key,
+    this.title,
+    required this.child,
+    this.showProfile = false,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +19,11 @@ class DefaultPage extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.cyan, useMaterial3: false),
       home: Scaffold(
         appBar: (title != null)
-            ? AppBar(centerTitle: true, title: Text(title!))
+            ? AppBar(
+                centerTitle: true,
+                actions: showProfile! ? [ProfileButton()] : null,
+                title: Text(title!),
+              )
             : null,
         body: SizedBox(child: Center(child: child)),
       ),
