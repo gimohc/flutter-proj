39081d68cdbc39dacc59c467a8476acd4c32d535
gimohc <mrlenovog50@gmail.com>
2025-11-20 23:34:42 +0300

    Defined all project routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:proj/pages/forgot_password.dart';
-import 'package:proj/pages/home.dart';
-import 'package:proj/pages/login.dart';
-import 'package:proj/pages/profile.dart';
-import 'package:proj/pages/register.dart';
+import 'package:proj/pages/auth/forgot_password.dart';
+import 'package:proj/pages/profile/adresses.dart';
+import 'package:proj/pages/profile/history.dart';
+import 'package:proj/pages/profile/information.dart';
+import 'package:proj/pages/profile/wallet.dart';
+import 'package:proj/pages/root/home.dart';
+import 'package:proj/pages/auth/login.dart';
+import 'package:proj/pages/root/profile.dart';
+import 'package:proj/pages/auth/register.dart';
+import 'package:proj/pages/support/about.dart';
+import 'package:proj/pages/support/help.dart';
 
 void main() {
   runApp(const MainApp());
@@ -15,12 +21,19 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.cyan, useMaterial3: false),
       routes: {
         "home": (context) => Home(),
         "profile": (context) => Profile(),
         "login": (context) => Login(),
         "register": (context) => Register(),
         "forgotPassword": (context) => ForgotPassword(),
+        "help": (context) => Help(),
+        "about": (context) => About(),
+        "wallet": (context) => Wallet(),
+        "information": (context) => Information(),
+        "history": (context) => History(),
+        "addresses": (context) => Addresses(),
       },
       home: Login(),
     );
